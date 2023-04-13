6918e2abf89dcbd7207b1202f03226ae1d0f258c
Jahid Eljah <jahideljah@gmail.com>
2023-04-13 23:47:45 +0200

    Navbar change and basic Main setup

---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+
+const Main = () => {
+  return (
+    <>
+      <h1>Main</h1>
+    </>
+  );
+};
+
+export default Main;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,12 @@ import {
   Box,
   Flex,
   useColorMode,
-  IconButton,
   Input,
   InputGroup,
   InputLeftElement,
+  Button,
 } from "@chakra-ui/react";
-import { BsToggleOn } from "react-icons/bs";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
 import { FaSearch } from "react-icons/fa";
 
 import logo from "../assets/react.svg";
@@ -16,54 +16,69 @@ function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding="1rem"
-      color="black"
-    >
-      <Box>
-        <img src={logo} alt="Logo" />
-      </Box>
-      <InputGroup width="80%">
-        <InputLeftElement pointerEvents="none" children={<FaSearch />} />
-        <Input
-          type="text"
-          placeholder="Search"
-          bg="#333333"
-          rounded="full"
-          overflow="hidden"
-          sx={{
-            '&[type="text"]': {
-              border: "none",
-              boxShadow: "none",
-              width: "100%",
-              _placeholder: {
-                color: "gray.500",
+    <>
+      <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        padding="1rem"
+      >
+        <Box>
+          <img src={logo} alt="Logo" />
+        </Box>
+        <InputGroup width="80%">
+          <InputLeftElement pointerEvents="none" children={<FaSearch />} />
+          <Input
+            type="text"
+            placeholder="Search"
+            rounded="full"
+            overflow="hidden"
+            sx={{
+              '&[type="text"]': {
+                width: "100%",
+                _placeholder: {
+                  color: "gray.500",
+                },
               },
-            },
-            _hover: {
-              bg: "#fff",
-            },
-            _focus: {
-              bg: "#fff",
-            },
-          }}
-        />
-      </InputGroup>
-
-      <IconButton
-        icon={<BsToggleOn onClick={toggleColorMode} />}
-        size="lg"
-        variant="ghost"
-        aria-label="Toggle navigation"
-        color="#fff"
-        sx={{ "&:hover": { bg: "transparent" } }}
-      />
-    </Flex>
+            }}
+          />
+        </InputGroup>
+        <Button
+          onClick={toggleColorMode}
+          variant="ghost"
+          aria-label="Toggle navigation"
+          sx={{ "&:hover": { bg: "transparent" } }}
+        >
+          {colorMode === "light" ? <BsFillMoonFill /> : <BsFillSunFill />}
+        </Button>
+      </Flex>
+    </>
   );
 }
 
 export default Navbar;
+
+// If you want the search-bar to look Identical with "rawg.io"
+// use the following code:
+{
+  /* <Input type="text" placeholder="Search" bg="#333333" rounded="full"
+  overflow="hidden"
+  sx={{
+    '&[type="text"]': {
+      border: "none",
+      boxShadow: "none",
+      width: "100%",
+      _placeholder: {
+        color: "gray.500",
+      },
+    },
+    _hover: {
+      bg: "#fff",
+    },
+    _focus: {
+      bg: "#fff",
+    },
+  }}
+/>; */
+}
