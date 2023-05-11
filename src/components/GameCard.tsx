557340e1870e67d91292de67fb1fabfcf4e8a16b
Jahid Eljah <jahideljah@gmail.com>
2023-05-11 23:31:29 +0200

    GameCard update

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,44 +1,45 @@
 import { SimpleGrid, Box, Image, Text } from "@chakra-ui/react";
+import data from "./data/genres";
 
 const GameCard = () => {
-  const items = [
-    { id: 1, name: "game1" },
-    { id: 2, name: "game2" },
-    { id: 3, name: "game3" },
-    { id: 4, name: "game4" },
-    { id: 5, name: "game5" },
-    { id: 6, name: "game6" },
-    { id: 7, name: "game7" },
-    { id: 8, name: "game8" },
-    { id: 9, name: "game9" },
-  ];
   return (
     <>
-      <SimpleGrid columns={{ sm: 2, md: 2, lg: 3 }} spacing={6}>
-        {items.map((item) => (
-          <Box
-            key={item.id}
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
+      <div>
+        {data.map((game) => (
+          <SimpleGrid
+            key={game.id}
+            columns={{ sm: 2, md: 2, lg: 3 }}
+            spacing={6}
           >
-            <Image src="https://via.placeholder.com/400x250" alt="game-image" />
-            <Box p="6">
-              <Box display="flex" alignItems="baseline">
-                <Text fontWeight="semibold" fontSize="lg" mr="2">
-                  {item.name}
-                </Text>
-                <Text fontSize="sm" color="gray.500">
-                  Platform
-                </Text>
+            {game.games.map((game) => (
+              <Box
+                key={game.id}
+                borderWidth="1px"
+                borderRadius="lg"
+                overflow="hidden"
+              >
+                <Image
+                  src="https://via.placeholder.com/400x250"
+                  alt="game-image"
+                />
+                <Box p="6">
+                  <Box display="flex" alignItems="baseline">
+                    <Text fontWeight="semibold" fontSize="lg" mr="2">
+                      {game.name}
+                    </Text>
+                    <Text fontSize="sm" color="gray.500">
+                      Platform
+                    </Text>
+                  </Box>
+                  <Text mt="2" fontSize="sm">
+                    Game description goes here...
+                  </Text>
+                </Box>
               </Box>
-              <Text mt="2" fontSize="sm">
-                Game description goes here...
-              </Text>
-            </Box>
-          </Box>
+            ))}
+          </SimpleGrid>
         ))}
-      </SimpleGrid>
+      </div>
     </>
   );
 };
