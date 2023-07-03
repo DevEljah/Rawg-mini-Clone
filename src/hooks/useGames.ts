7ab15ac89af6c3291c738cd3aaff376b32182494
Jahid Eljah <jahideljah@gmail.com>
2023-07-03 23:09:46 +0200

    error cancellation added

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import apiClient from "../utils/apiClient";
+import { CanceledError } from "axios";
 
 interface Game {
   id: number;
@@ -14,15 +15,22 @@ interface FetchGamesRes {
 }
 
 const useGames = () => {
+  const controller = new AbortController();
+
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
     apiClient
-      .get<FetchGamesRes>("/games")
+      .get<FetchGamesRes>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
     console.log(games);
+
+    return () => controller.abort();
   }, []);
   return { games, error };
 };
