e4233c9c33e51b5d3b51166e236df17819d88f84
Jahid Eljah <jahideljah@gmail.com>
2023-07-25 21:27:16 +0200

    Platforms selection bug fix

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const GameHeading = ({ gameQuery }: Props) => {
   const heading = `${gameQuery.platform?.name || ""} ${
-    gameQuery.genre?.name
+    gameQuery.genre?.name || ""
   } Games`;
 
   return (
